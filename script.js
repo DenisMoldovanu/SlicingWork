fab45115411bf8ab37db5a85248b54ccfb7e8d8f
Denis <denis.moldovanu@creativsoft.md>
2023-12-15 22:45:07 +0200

    style updates

--- script.js
+++ script.js
@@ -13,3 +13,32 @@ const swiper = new Swiper(".swiper", {
         prevEl: ".swiper-button-prev",
     },
 });
+// Get references to the elements
+const burgerBtn = document.querySelector(".burger-btn");
+const closeBtn = document.querySelector(".close-btn");
+const mobileNav = document.querySelector(".mobile-nav");
+const body = document.querySelector("body");
+
+// Function to add 'active' class to mobile-nav
+function openMobileNav() {
+    mobileNav.classList.add("active");
+    body.classList.add("overflow");
+}
+
+// Function to remove 'active' class from mobile-nav
+function closeMobileNav() {
+    mobileNav.classList.remove("active");
+    body.classList.remove("overflow");
+}
+
+// Event listener for burger button click
+burgerBtn.addEventListener("click", openMobileNav);
+
+// Event listener for close button click
+closeBtn.addEventListener("click", closeMobileNav);
+
+window.addEventListener("resize", () => {
+    if (window.innerWidth > 992) {
+        body.classList.remove("overflow");
+    }
+});
