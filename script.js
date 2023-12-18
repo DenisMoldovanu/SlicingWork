6c708da6aa22125e5c6ba06d70a28aaa1abc40f1
Denis <denis.moldovanu@creativsoft.md>
2023-12-18 09:45:21 +0200

    update changes on git

--- script.js
+++ script.js
@@ -51,3 +51,38 @@ document.addEventListener("click", function (event) {
         closeMobileNav();
     }
 });
+
+
+const clickBlock = document.querySelector('.click-block');
+const variants = document.querySelectorAll('.hero-img-variant');
+
+clickBlock?.addEventListener('click', () => {
+    const activeVariant = document.querySelector('.hero-img-variant.active');
+    const activeIndex = parseInt(activeVariant.classList[1].replace('variant', ''));
+
+    variants.forEach(variant => variant.classList.remove('active'));
+
+    if (activeIndex === 3) {
+        // Set 'active' class to variant4 when clicking variant3
+        const variant4 = document.querySelector('.variant4');
+        variant4.classList.add('active');
+
+        // After 2 seconds, change to variant5
+        setTimeout(() => {
+            variant4.classList.remove('active');
+            const variant5 = document.querySelector('.variant5');
+            variant5.classList.add('active');
+            clickBlock.className = 'click-block variant5';
+        }, 2000);
+    } else {
+        // Calculate the index of the next variant
+        const nextIndex = (activeIndex % variants.length) + 1;
+
+        // Set 'active' class to the next variant
+        const nextVariant = document.querySelector(`.variant${nextIndex}`);
+        nextVariant.classList.add('active');
+
+        // Set click-block class to the name of the active variant
+        clickBlock.className = `click-block variant${nextIndex}`;
+    }
+});
