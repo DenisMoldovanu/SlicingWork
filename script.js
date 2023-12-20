c85e86af5d5aed0cd9148b14c0422ee2fd6ea3a5
Denis <93701844+DenisMoldovanu@users.noreply.github.com>
2023-12-20 12:22:12 +0200

    set images from javascript

--- script.js
+++ script.js
@@ -52,27 +52,28 @@ document.addEventListener("click", function (event) {
     }
 });
 
+const clickBlock = document.querySelector(".click-block");
+const variants = document.querySelectorAll(".hero-img-variant");
 
-const clickBlock = document.querySelector('.click-block');
-const variants = document.querySelectorAll('.hero-img-variant');
+clickBlock?.addEventListener("click", () => {
+    const activeVariant = document.querySelector(".hero-img-variant.active");
+    const activeIndex = parseInt(
+        activeVariant.classList[1].replace("variant", "")
+    );
 
-clickBlock?.addEventListener('click', () => {
-    const activeVariant = document.querySelector('.hero-img-variant.active');
-    const activeIndex = parseInt(activeVariant.classList[1].replace('variant', ''));
-
-    variants.forEach(variant => variant.classList.remove('active'));
+    variants.forEach((variant) => variant.classList.remove("active"));
 
     if (activeIndex === 3) {
         // Set 'active' class to variant4 when clicking variant3
-        const variant4 = document.querySelector('.variant4');
-        variant4.classList.add('active');
+        const variant4 = document.querySelector(".variant4");
+        variant4.classList.add("active");
 
         // After 2 seconds, change to variant5
         setTimeout(() => {
-            variant4.classList.remove('active');
-            const variant5 = document.querySelector('.variant5');
-            variant5.classList.add('active');
-            clickBlock.className = 'click-block variant5';
+            variant4.classList.remove("active");
+            const variant5 = document.querySelector(".variant5");
+            variant5.classList.add("active");
+            clickBlock.className = "click-block variant5";
         }, 2000);
     } else {
         // Calculate the index of the next variant
@@ -80,9 +81,160 @@ clickBlock?.addEventListener('click', () => {
 
         // Set 'active' class to the next variant
         const nextVariant = document.querySelector(`.variant${nextIndex}`);
-        nextVariant.classList.add('active');
+        nextVariant.classList.add("active");
 
         // Set click-block class to the name of the active variant
         clickBlock.className = `click-block variant${nextIndex}`;
     }
 });
+
+// Array of image sources
+const imageSources = [
+    "images/Brandlogos/1.png",
+    "images/Brandlogos/2.png",
+    "images/Brandlogos/3.png",
+    "images/Brandlogos/4.png",
+    "images/Brandlogos/5.png",
+    "images/Brandlogos/6.png",
+    "images/Brandlogos/7.png",
+    "images/Brandlogos/8.png",
+    "images/Brandlogos/9.png",
+    "images/Brandlogos/10.png",
+    "images/Brandlogos/11.png",
+    "images/Brandlogos/12.png",
+    "images/Brandlogos/13.png",
+    "images/Brandlogos/14.png",
+    "images/Brandlogos/15.png",
+    "images/Brandlogos/16.png",
+    "images/Brandlogos/17.png",
+    "images/Brandlogos/18.png",
+    "images/Brandlogos/19.png",
+    "images/Brandlogos/20.png",
+    "images/Brandlogos/21.png",
+    "images/Brandlogos/22.png",
+    "images/Brandlogos/23.png",
+    "images/Brandlogos/24.png",
+    "images/Brandlogos/25.png",
+    "images/Brandlogos/26.png",
+    "images/Brandlogos/27.png",
+    "images/Brandlogos/28.png",
+    "images/Brandlogos/29.png",
+    "images/Brandlogos/30.png",
+    "images/Brandlogos/31.png",
+    "images/Brandlogos/32.png",
+    "images/Brandlogos/33.png",
+    "images/Brandlogos/34.png",
+    "images/Brandlogos/35.png",
+    "images/Brandlogos/36.png",
+    "images/Brandlogos/37.png",
+    "images/Brandlogos/38.png",
+    "images/Brandlogos/39.png",
+    "images/Brandlogos/40.png",
+    "images/Brandlogos/41.png",
+    "images/Brandlogos/42.png",
+    "images/Brandlogos/43.png",
+    "images/Brandlogos/44.png",
+    "images/Brandlogos/45.png",
+    "images/Brandlogos/46.png",
+    "images/Brandlogos/47.png",
+    "images/Brandlogos/48.png",
+    "images/Brandlogos/49.png",
+    "images/Brandlogos/50.png",
+    "images/Brandlogos/51.png",
+    "images/Brandlogos/52.png",
+    "images/Brandlogos/53.png",
+    "images/Brandlogos/54.png",
+    "images/Brandlogos/55.png",
+    "images/Brandlogos/56.png",
+    "images/Brandlogos/57.png",
+    "images/Brandlogos/58.png",
+    "images/Brandlogos/59.png",
+    "images/Brandlogos/60.png",
+    "images/Brandlogos/61.png",
+    "images/Brandlogos/62.png",
+    "images/Brandlogos/63.png",
+    "images/Brandlogos/64.png",
+    // -----------------------------
+    "images/Brandlogos/1.png",
+    "images/Brandlogos/2.png",
+    "images/Brandlogos/3.png",
+    "images/Brandlogos/4.png",
+    "images/Brandlogos/5.png",
+    "images/Brandlogos/6.png",
+    "images/Brandlogos/7.png",
+    "images/Brandlogos/8.png",
+    "images/Brandlogos/9.png",
+    "images/Brandlogos/10.png",
+    "images/Brandlogos/11.png",
+    "images/Brandlogos/12.png",
+    "images/Brandlogos/13.png",
+    "images/Brandlogos/14.png",
+    "images/Brandlogos/15.png",
+    "images/Brandlogos/16.png",
+    "images/Brandlogos/17.png",
+    "images/Brandlogos/18.png",
+    "images/Brandlogos/19.png",
+    "images/Brandlogos/20.png",
+    "images/Brandlogos/21.png",
+    "images/Brandlogos/22.png",
+    "images/Brandlogos/23.png",
+    "images/Brandlogos/24.png",
+    "images/Brandlogos/25.png",
+    "images/Brandlogos/26.png",
+    "images/Brandlogos/27.png",
+    "images/Brandlogos/28.png",
+    "images/Brandlogos/29.png",
+    "images/Brandlogos/30.png",
+    "images/Brandlogos/31.png",
+    "images/Brandlogos/32.png",
+    "images/Brandlogos/33.png",
+    "images/Brandlogos/34.png",
+    "images/Brandlogos/35.png",
+    "images/Brandlogos/36.png",
+    "images/Brandlogos/37.png",
+    "images/Brandlogos/38.png",
+    "images/Brandlogos/39.png",
+    "images/Brandlogos/40.png",
+    "images/Brandlogos/41.png",
+    "images/Brandlogos/42.png",
+    "images/Brandlogos/43.png",
+    "images/Brandlogos/44.png",
+    "images/Brandlogos/45.png",
+    "images/Brandlogos/46.png",
+    "images/Brandlogos/47.png",
+    "images/Brandlogos/48.png",
+    "images/Brandlogos/49.png",
+    "images/Brandlogos/50.png",
+    "images/Brandlogos/51.png",
+    "images/Brandlogos/52.png",
+    "images/Brandlogos/53.png",
+    "images/Brandlogos/54.png",
+    "images/Brandlogos/55.png",
+    "images/Brandlogos/56.png",
+    "images/Brandlogos/57.png",
+    "images/Brandlogos/58.png",
+    "images/Brandlogos/59.png",
+    "images/Brandlogos/60.png",
+    "images/Brandlogos/61.png",
+    "images/Brandlogos/62.png",
+    "images/Brandlogos/63.png",
+    "images/Brandlogos/64.png",
+];
+// Function to display images
+function displayImages() {
+    const brandsList = document.getElementById("brandsList");
+
+    // Loop through the imageSources array
+    imageSources.forEach((src) => {
+        // Create an img element for each image source
+        const img = document.createElement("img");
+        img.src = src;
+        img.alt = "Brand Logo"; // Set alt attribute for accessibility
+
+        // Append the img element to the brandsList div
+        brandsList.appendChild(img);
+    });
+}
+
+// Call the function to display images
+displayImages();
